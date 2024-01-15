50ee218e7ce833790d1499985f96eb522ffae3b5
Christophe <pc57plv@eduvaud.ch>
2024-01-15 19:48:13 +0100

    Update of the DB

--- Script DATABASE avec MLD.sql
+++ Script DATABASE avec MLD.sql
@@ -7,7 +7,7 @@ SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='ONLY_FULL_GROUP_BY,STRICT_TRANS_TABLES,N
 -- -----------------------------------------------------
 -- Schema ProjetDBPY
 -- -----------------------------------------------------
-DROP DATABASE projetdbpy;
+
 -- -----------------------------------------------------
 -- Schema ProjetDBPY
 -- -----------------------------------------------------
@@ -21,7 +21,7 @@ CREATE TABLE IF NOT EXISTS `ProjetDBPY`.`Players` (
   `id` INT NOT NULL AUTO_INCREMENT,
   `alias` VARCHAR(30) NOT NULL,
   `password` VARCHAR(100) NOT NULL,
-  `student` BINARY(1) NOT NULL DEFAULT 0,
+  `level` BINARY(1) NOT NULL DEFAULT 0,
   PRIMARY KEY (`id`),
   UNIQUE INDEX `alias_UNIQUE` (`alias` ASC) VISIBLE)
 ENGINE = InnoDB;
