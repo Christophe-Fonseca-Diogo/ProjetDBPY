e4f5500a93859ae9a4c1328cdd1fc4f351e230b2
Christophe <pc57plv@eduvaud.ch>
2024-01-15 19:49:54 +0100

    Update of the DB

--- Script DATABASE avec MLD.sql
+++ Script DATABASE avec MLD.sql
@@ -7,7 +7,7 @@ SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='ONLY_FULL_GROUP_BY,STRICT_TRANS_TABLES,N
 -- -----------------------------------------------------
 -- Schema ProjetDBPY
 -- -----------------------------------------------------
-
+DROP DATABASE projetdbpy;
 -- -----------------------------------------------------
 -- Schema ProjetDBPY
 -- -----------------------------------------------------
